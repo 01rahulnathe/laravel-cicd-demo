dfceca172bb22ef47d4c5e1727e73803435ebe20
Rahul Nathe <01rahulnathe@gmail.com>
2026-06-02 14:42:53 +0530

    Add debug step

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,4 +5,14 @@ namespace App\Http\Controllers;
 abstract class Controller
 {
     //
+    /*(c) Microsoft Corporation. All rights reserved.
+
+C:\Users\rahul.nathe>ssh-keygen -t ed25519 -f github-action
+Generating public/private ed25519 key pair.
+Enter passphrase (empty for no passphrase):
+Enter same passphrase again:
+Your identification has been saved in github-action
+Your public key has been saved in github-action.pub
+The key fingerprint is:
+SHA256:sUdd0IWOaEn1tnsls4hc1gsON7HFX2v95c8h8TDXBXM mahaitgov\rahul.nathe@MIT-APJ-046 */
 }
